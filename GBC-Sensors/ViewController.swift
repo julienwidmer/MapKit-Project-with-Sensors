4d4e1f6098751a0668f65c86984ada942429d867
Julien Widmer <ju.widmer@pm.me>
2023-03-09 15:05:10 -0500

    Initial Commit

---
+++ GBC-Sensors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBC-Sensors
+//
+//  Created by Julien Widmer on 2023-03-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
